a0adab30b943baeccaf65518ca1e483fc8132b27
Anthony Carton <mr.carton@gmail.com>
2018-08-12 12:13:51 -0600

    Pseudocode for Enemy Class

--- js/app.js
+++ js/app.js
@@ -11,15 +11,25 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // TODO: multiply any movement by the dt parameter to ensure the game runs at the same speed for all computers
+
+    // TODO: Update the Enemy location
+
+    // TODO: Handle collision with the Player
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+Enemy.prototype.initialLocation = function() {
+  // TODO: Set Enemy Initial Location
+}
+
+Enemy.prototype.speed = function() {
+  // TODO: Set variable enemy speed
+}
 
 // Now write your own player class
 class Player {
