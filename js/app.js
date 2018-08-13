4a26186ce3b09f5cd4bf84934ca96370e2eed3f5
Anthony Carton <mr.carton@gmail.com>
2018-08-13 12:46:16 -0600

    corrected set speed per project instructions

--- js/app.js
+++ js/app.js
@@ -3,14 +3,11 @@ class Enemy {
     this.sprite = 'images/enemy-bug.png'; // TODO: random enemy f()
     this.x = -75;
     this.y = initialLocation();
+    this.speed = 400;  // TODO: Set VARIABLE enemy speed
   }
   update(dt){
-    // TODO: Set VARIABLE enemy speed
-    let speed = dt*200;
-    let newX = this.x + speed;
-
-    // TODO: Update the Enemy location
-    this.x = newX;
+    // Update the Enemy location
+    this.x = this.x + (dt*this.speed);
 
     // TODO: Handle collision with the Player
 
@@ -35,24 +32,44 @@ class Player {
     this.sprite = 'images/char-boy.png';
   }
   // Methods: This class requires an update(), render() and a handleInput() method.
-  update() {
-
+  update(dt) {
+    // engine.js passes dt into update(dt)
+    // TODO: Update the player location
+    // this.x = this.x + moveDef[keyInput];
+    // this.y = this.y + moveDef[keyInput];
   }
   render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    // TODO, could this just also use the above?
-  }
-  handleInput() {
+    // console.log(`before ${this.x} and ${this.y}`);
+    // player.handleInput();
 
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    // console.log(`after ${this.x} and ${this.y}`);
+  }
+  handleInput(keyInput) {
+    let move = movePlayer(keyInput);
+    console.log(move);
   }
 }
 
+let movePlayer = function(keyInput) {
+  console.log(keyInput);
+  let increment = 10;
+
+  const moveDef = {
+    left: function(){ this.x = this.x - increment},
+    right: function(){ this.x = this.x + increment},
+    up: function(){ this.y = this.y + increment},
+    down: function(){ this.y = this.y - increment}
+    }
+  return moveDef[keyInput]();
+}
+
 let initialLocation = function (){
+  // Replaced switch statement with laneDef Object Literal. https://toddmotto.com/deprecating-the-switch-statement-for-object-literals/
   const laneDef = { 0 : 60, 1 : 145, 2 : 225 };
   let rand = (function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   })(3);
-  // Replaced switch statement with laneDef Object Literal. https://toddmotto.com/deprecating-the-switch-statement-for-object-literals/
   return laneDef[rand];
 };
 
@@ -88,12 +105,15 @@ window.addEventListener('load', function(event){
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
-
+  var allowedKeys = {
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
+  };
+  // let keyCheck = Object.keys(allowedKeys).includes(e.keyCode.toString());
+  // if (keyCheck == true) {
+  if (Object.keys(allowedKeys).includes(e.keyCode.toString()) == true) {
     player.handleInput(allowedKeys[e.keyCode]);
+  }
 });
