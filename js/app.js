953c8a87b31ec0062c7c6eb963985ef9c4e851cd
Anthony Carton <mr.carton@gmail.com>
2018-08-13 12:49:58 -0600

    clean up format

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ class Enemy {
 
     // TODO: Handle collision with the Player
 
-    // Kill bug after it leaves screen
+    // Remove bug from array after it leaves screen
     if (this.x > 600) {
       killBug();
     }
